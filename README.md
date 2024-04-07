65a895d2ba4ba40bf364696cf87ebfeffaeb4f5e
sHKamil <shootekb@gmail.com>
2024-04-07 23:44:59 +0200

    readme

---
+++ README.md
@@ -0,0 +1,8 @@
+## Installation
+Docker required
+
+Linux/WSL2
+run:
+```
+HOST_USER_ID=$(id -u) docker compose up -d --build
+```
--- backend/index.php
+++ backend/index.php
@@ -2,6 +2,8 @@
 
 require 'vendor/autoload.php';
 
+header("Access-Control-Allow-Origin: *");
+
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $router = new App\Router\Router;
 $routes = require_once 'app/Router/web.php';
